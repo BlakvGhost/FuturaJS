f5c93bf92b8c720bbe2e6c447c4a587a2cb1e2ca
BlakvGhost <kabirou2001@gmail.com>
2023-01-01 13:15:07 +0000

    first commit

--- fjs.init.js
+++ fjs.init.js
@@ -2,5 +2,5 @@ import Sticky from "./lib/core/Sticky.js";
 import Intersection from "./lib/core/Intersection.js";
 import NumberAutoCount from "./lib/core/NumberAutoCount.js";
 import AutoWriteText from "./lib/core/AutoWriteText.js";
-// import Tab from "./lib/core/Tab.js";
+import Tab from "./lib/core/Tab.js";
 import FvCarousel from "./lib/FvCarousel/FvCarousel.js";
